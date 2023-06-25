3293b92c66147436913ab87b5a2c6b9cd057cabf
farel <2101020057@student.umrah.ac.id>
2023-06-25 20:42:53 +0700

    Menu Bar Admin

--- admin_page/status_perjalanan.php
+++ admin_page/status_perjalanan.php
@@ -16,10 +16,10 @@ if(!isset($_SESSION['admin_name'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Halaman Dashboard Admin</title>
+   <title>Halaman Data Kurir Admin</title>
 
    <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/admin_page_style.css">
+   <link rel="stylesheet" href="css/status_perjalanan.css">
 
 </head>
 <body>
@@ -28,6 +28,7 @@ if(!isset($_SESSION['admin_name'])){
    <div class="kotak">
       <div class="header">
          <img src="../admin_page/asset/logo.png" alt="logo">
+         <h2>Selamat Datang, Administrator</h2>
       </div>
       <div class="menu_bar">
          <img src="../admin_page/asset/profile.png" alt="profile">
@@ -35,48 +36,26 @@ if(!isset($_SESSION['admin_name'])){
          <div class="nama_admin"><?php echo $_SESSION['admin_name'] ?></div>
          <h2>Menu</h2>
          <div class="menubar1">
-            <h3><a href="../admin_page/admin_page.php">Dashboard</a></h3>
+         <h3><a href="../admin_page/admin_page.php">Dashboard</a></h3>
             <img src="../admin_page/asset/dashboard.png" alt="">
          </div>
          <div class="menubar2">
-            <h3><a href="../admin_page/data_barang.php">Data Barang</a></h3>
+         <h3><a href="../admin_page/data_barang.php">Data Barang</a></h3>
             <img src="../admin_page/asset/barang.png" alt="">
          </div>
          <div class="menubar3">
-            <h3><a href="../admin_page/status_perjalanan.php">Status Perjalanan</a></h3>
+         <h3><a href="../admin_page/status_perjalanan.php">Status Perjalanan</a></h3>
             <img src="../admin_page/asset/status_perjalanan.png" alt="">
          </div>
          <div class="menubar4">
-            <h3><a href="../admin_page/data_kurir.php">Data Kurir</a></h3>
+         <h3><a href="../admin_page/data_kurir.php">Data Kurir</a></h3>
             <img src="../admin_page/asset/kurir.png" alt="">
          </div>
       </div>
    </div>
 
    <div class="content">
-      <div class="kotak1"></div>
-      <h3>Hi !</h3>
-      <div class="welcome">Selamat Datang, Administrator</div>
-      <div class="kotak2">
-         <img src="../admin_page/asset/barang.png" alt="">
-         <h3>Data Barang</h3>
-      </div>
-      <div class="kotak3">
-         <img src="../admin_page/asset/status_perjalanan.png" alt="">
-         <div class="tulisan">
-            <h3>Status</h3>
-            <h4>Perjalanan</h4>
-         </div>
-      </div>
-      <div class="kotak4">
-         <img src="../admin_page/asset/kurir.png" alt="">
-         <h3>Data Kurir</h3>
-      </div>
-      <div class="kotak_logout"></div>
-      <div class="logout">
-         <img src="../admin_page/asset/logout.png" alt="">
-         <a href="../login/logout.php" class="btn">Logout</a>
-      </div>
+      
    </div>
 
 </div>
