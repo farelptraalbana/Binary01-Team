5521f172d1bdc5b95b3a284e3a041f591d59fc74
farel <2101020057@student.umrah.ac.id>
2023-06-30 15:15:12 +0700

    kurir menu 2

--- Dashboard/dashboard.php
+++ Dashboard/dashboard.php
@@ -2,41 +2,6 @@
 
 @include 'config.php';
 
-session_start();
-
-if(isset($_POST['submit'])){
-
-   $name = mysqli_real_escape_string($conn, $_POST['name']);
-   $email = mysqli_real_escape_string($conn, $_POST['email']);
-   $pass = md5($_POST['password']);
-   $cpass = md5($_POST['cpassword']);
-   $user_type = $_POST['user_type'];
-
-   $select = " SELECT * FROM user_form WHERE email = '$email' && password = '$pass' ";
-
-   $result = mysqli_query($conn, $select);
-
-   if(mysqli_num_rows($result) > 0){
-
-      $row = mysqli_fetch_array($result);
-
-      if($row['user_type'] == 'admin'){
-
-         $_SESSION['admin_name'] = $row['name'];
-         header('location:../admin_page/admin_page.php');
-
-      }elseif($row['user_type'] == 'kurir'){
-
-         $_SESSION['kurir_name'] = $row['name'];
-         header('location:../kurir_page/kurir_page.php');
-
-      }
-     
-   }else{
-      $error[] = 'incorrect email or password!';
-   }
-
-};
 ?>
 
 <!DOCTYPE html>
@@ -98,7 +63,7 @@ if(isset($_POST['submit'])){
         <div class="input-group mb-3 mt-3" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
           <form action="" method="post" class="mx-auto w-50" >
             <input type="text" class="form-control text-center" placeholder="Masukkan Nomor Resi Anda" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" style="background-color: #FFBA08; border: none;">
-            <button type="submit" name="submit" class="btn mt-4 d-block mx-auto" style="background-color: #FFBA08; border-radius: 40px; width: 100px;"><a href="">Search</a></button>
+            <button type="submit" name="submit" class="btn mt-4 d-block mx-auto" style="background-color: #FFBA08; border-radius: 40px; width: 100px;"><a href="../Dashboard/lacak_kiriman.php">Search</a></button>
           </form>
         </div>
       </div>     
